28bc67eac73b3b80979319c77a59ef80c9a205a7
bhaskarfulara <bhaskar.fulara@tothenew.com>
2022-04-05 02:09:53 +0530

    adding navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import './Header.css'
-
+import {Link} from 'react-router-dom'
+import { RiMessengerLine } from 'react-icons/ri';
+import {TiUserAddOutline} from 'react-icons/ti'
+import {CgLogOff} from 'react-icons/cg'
 function Header() {
   return (
-    <div className='headerPart'>
-      <div className='left'>
-        <ul>
-          <li><h1>
-            Logo</h1></li>
-        </ul>
-      </div>
-      <div className='Right'>
-        <ul>
-          <li>Home</li>
-          <li>Profile</li>
-          <li>image</li>
-        </ul>
-      </div>
-    </div>
+    <nav>
+        <div className="left">
+            <img className="logo"
+                src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1473843963/cdy69xpmmkjntymhbxpa.png"
+                alt="" style={{width:"80px",height:"80px"}}/>
+        </div>
+        <div className="right">
+            <ul className="rightComp beforeHide">
+                <li><Link><img className="avtarLogo" src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2021/12/10/16391545485537.jpg" alt="Avatar"/></Link></li>
+                <li className="AccountHolderName" style={{width:"auto"}}>Bhaskar Chandar</li>
+                <li><Link><RiMessengerLine style={{width:"40px",height:"40px",color:"black"}}/></Link></li>
+                <li><Link><TiUserAddOutline style={{width:"40px",height:"40px",color:"black"}}/></Link></li>
+                <li><Link><CgLogOff style={{width:"40px",height:"40px",color:"black"}}/></Link></li>
+                
+            </ul>
+            <div className="afterHide">
+            <Link><img className="messenger" src="./menu.png" alt=""/></Link>
+            </div>
+        </div>
+    </nav>
   )
 }
 
